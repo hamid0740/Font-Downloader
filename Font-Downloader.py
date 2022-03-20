0ca1464d7a9356dbd6102f42cddce303212ecf1a
Hamidreza Dehghan <hamidd0740@gmail.com>
2022-03-20 23:15:50 +0330

    v1.4

--- Font-Downloader.py
+++ Font-Downloader.py
@@ -12,14 +12,14 @@ class color:
 	
 #App info
 print(color.purple + "App name:  " + color.reset + "Font Downloader")
-print(color.purple + "Version:   " + color.reset + "1.3")
+print(color.purple + "Version:   " + color.reset + "1.4")
 print(color.purple + "Developer: " + color.reset + "hamid0740")
 print(color.purple + "GitHub:    " + color.reset + "https://github.com/hamid0740/Font-Downloader" + "\n")
 
 #Defining temporary variables for weight names
-weights1 = ["Hairline", "Book", "Demi", "Regular", "Normal", "Text", "Medium", "Heavy", "Fat", "Poster"] #Weight names that don't ever get Extra or Ultra
+weights1 = ["Hairline", "Line", "Book", "News", "Demi", "Regular", "Normal", "Text", "Medium", "Heavy", "Mass", "Fat", "Poster"] #Weight names that don't ever get Extra or Ultra
 weights2 = ["Thin", "Light", "Lite", "Thick", "Bold", "Dark", "Black"] #Weight names that may get Extra or Ultra
-weights3 = ["Bold"] #Weight names that may get Semi or Demi
+weights3 = ["Light", "Bold"] #Weight names that may get Semi or Demi
 weights2_temp = []
 for i in range(len(weights2)):
 	weights2_temp.append(weights2[i])
@@ -31,6 +31,10 @@ for i in range(len(weights2)):
 	weights2_temp.append("Ultra" + weights2[i].lower())
 	weights2_temp.append("Ultra-" + weights2[i])
 	weights2_temp.append("Ultra-" + weights2[i].lower())
+	weights2_temp.append("Very" + weights2[i])
+	weights2_temp.append("Very" + weights2[i].lower())
+	weights2_temp.append("Very-" + weights2[i])
+	weights2_temp.append("Very-" + weights2[i].lower())
 weights3_temp = []
 for i in range(len(weights3)):
 	weights3_temp.append(weights3[i])
@@ -51,7 +55,7 @@ formats = ["ttf", "woff", "woff2", "eot", "svg"]
 
 print(color.red + "✗ WRONG: https://example.com/font/Arial-Regular.ttf \n" + color.green + "✓ RIGHT: https://example.com/font/Arial- ")
 main_url = input(color.yellow + "Enter the font file URL without weight name and file format: " + color.reset)
-print(color.purple + "\n's' for Standard case (81 weight names)\n" + "'l' for Lower case (49 weight names)\n" + "'u' for Upper case (49 weight names)\n" + "'all' for All cases (179 weight names)\n" + color.blue + "You can combine letters for combined cases. like 'lu' to have Lower and Upper cases.")
+print(color.purple + "\n's' for Standard case (120 weight names)\n" + "'l' for Lower case (70 weight names)\n" + "'u' for Upper case (70 weight names)\n" + "'all' for All cases (260 weight names)\n" + color.blue + "You can combine letters for combined cases. like 'lu' to have Lower and Upper cases.")
 weights_case = input(color.yellow + "As you've inspected font URLs, how is their weight name case? " + color.reset)
 weights = []
 if "all" in weights_case.lower():
@@ -72,10 +76,12 @@ def dl(url):
 	global count
 	global count_d
 	name = url.split("/")[len(url.split("/")) - 1]
-	response = requests.head(url)
+	response = requests.head(url, allow_redirects = True)
 	if not os.path.exists(font_name + "/" + font_name + "-Links.txt"):
 		open(font_name + "/" + font_name + "-Links.txt", "a").write("")
-	if response.status_code == 200:
+	if not os.path.exists(font_name + "/" + font_name + "-Links-All.txt"):
+		open(font_name + "/" + font_name + "-Links-All.txt", "a").write("")
+	if response.status_code in range(200, 299) and not response.headers["content-type"] in ["text/html"]:
 		if not url.lower() in open(font_name + "/" + font_name + "-Links.txt").read().lower():
 			request = requests.get(url, allow_redirects = True)
 			open(font_name + "/" + name, 'wb').write(request.content)
@@ -90,21 +96,28 @@ def dl(url):
 		print(color.red + "[✗] " + name +" | (404) Not found!")
 	elif response.status_code == 408:
 		print(color.red + "[✗] " + name +" | (408) Request time out!")
+	elif response.headers["content-type"] in ["text/html"]:
+		print(color.red + "[✗] " + name +" | Returned HTML page!")
 	else:
 		print(color.red + "[✗] " + name +" | (" + str(response.status_code) + ") Problem!")
+	open(font_name + "/" + font_name + "-Links-All.txt", "a").write(url + "\n")
 	count += 1
 		
 
 #Starting inspection and downloading process
 font_name = main_url.split("/")[-1]
-if font_name[-1] == "-" or font_name[-1] == "_" or font_name[-1] == " ":
+if font_name[-1] in ["-", "_", " "]:
 	font_name = font_name[:-1]
 if not os.path.exists(font_name):
 	os.makedirs(font_name)
+if os.path.exists(font_name + "/" + font_name + "-Links.txt"):
+	open(font_name + "/" + font_name + "-Links.txt", "r+").truncate(0)
+if os.path.exists(font_name + "/" + font_name + "-Links-All.txt"):
+	open(font_name + "/" + font_name + "-Links-All.txt", "r+").truncate(0)
 #Check for font file without any weight name
 start_time = time.time_ns()
 for j in range(len(formats)):
-	if main_url[-1] == "-" or main_url[-1] == "_" or main_url[-1] == " ":
+	if main_url[-1]  in ["-", "_", " "]:
 		url = main_url[:-1] + "." + formats[j]
 	else:
 		url = main_url + "." + formats[j]
@@ -115,18 +128,24 @@ for i in range(len(weights)):
 		url = main_url + weights[i] + "." + formats[j]
 		dl(url)
 end_time = time.time_ns()
-processing_time = round((end_time - start_time) / (10**9), 2)
+processing_time = (end_time - start_time) / (10**9)
 pt_min, pt_sec = divmod(processing_time, 60)
+pt_sec = round(pt_sec, 2)
 #remove extra empty line in Links.txt file
 with open(font_name + "/" + font_name + "-Links.txt", "r+") as links_file:
 	links_txt = links_file.read().rstrip()
 	links_file.truncate(0)
 	links_file.write(links_txt)
+#remove extra empty line in Links-all.txt file
+with open(font_name + "/" + font_name + "-Links-All.txt", "r+") as links_all_file:
+	links_all_txt = links_all_file.read().rstrip()
+	links_all_file.truncate(0)
+	links_all_file.write(links_all_txt)
 
 print(color.yellow + "\n[!] Results:")
 if pt_min == 0:
 	print(color.blue + "[!] Processing time: " + color.yellow + str(pt_sec) + " seconds")
 else:
-	print(color.blue + "[!] Processing time: " + color.yellow + str(pt_min) + " minutes and " + str(pt_sec) + " seconds")
+	print(color.blue + "[!] Processing time: " + color.yellow + str(int(pt_min)) + " minutes and " + str(pt_sec) + " seconds")
 print(color.blue + "[!] Downloaded: " + color.yellow + str(count_d) + " files")
 print(color.blue + "[!] Inspected: " + color.yellow + str(count) + " files")
